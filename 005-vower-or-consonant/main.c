28ccadc7141b8a3c6391c1e6b7345424fe95157a
Antu Saha <imantu09@gmail.com>
2023-05-25 22:01:18 +0600

    005-vower-or-consonant

---
+++ 005-vower-or-consonant/main.c
@@ -0,0 +1,41 @@
+/**
+ * Problem: Take a character and determine wether it is a vower or consonant
+ * neutral
+ * ----------------------------------------------
+ * Input:
+ * Enter the character: a
+ *
+ * Output:
+ * Vowel
+ * ----------------------------------------------
+ * Input:
+ * Enter the character: d
+ *
+ * Output:
+ * Consonant
+ * ----------------------------------------------
+ * Input:
+ * Enter the character: A
+ *
+ * Output:
+ * Vowel
+ * ----------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main() {
+  char ch;
+
+  printf("Enter the character: ");
+  scanf("%c", &ch);
+
+  if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+      ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+    printf("Vowel\n");
+  } else {
+    printf("Consonant\n");
+  }
+
+  return 0;
+}
