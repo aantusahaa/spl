546747edef43e3be9108868d292bc1780fff4fbc
Antu Saha <imantu09@gmail.com>
2023-08-08 10:22:48 +0600

    add: exponent function

---
+++ 016-exponent-function/main.c
@@ -0,0 +1,32 @@
+/**
+ * Problem: Write a function to calculate exponent of a number.
+ */
+
+#include <stdio.h>
+
+double power(double base, int exponent) {
+  double result = 1.0;
+
+  for (int i = 0; i < exponent; i++) {
+    result *= base;
+  }
+
+  return result;
+}
+
+int main() {
+  double base;
+  int exponent;
+
+  printf("Enter the base: ");
+  scanf("%lf", &base);
+
+  printf("Enter the exponent: ");
+  scanf("%d", &exponent);
+
+  double result = power(base, exponent);
+
+  printf("%.2lf ^ %d = %.2lf\n", base, exponent, result);
+
+  return 0;
+}
