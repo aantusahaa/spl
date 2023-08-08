4c7158f72c8e7da50708d7e33a2a1ad62606cd78
Antu Saha <imantu09@gmail.com>
2023-08-08 11:54:33 +0600

    add: string concatenation

---
+++ 021-concatenate-strings/main.c
@@ -0,0 +1,38 @@
+/**
+ * Problem: Input two strings variables str1[] and str2[]. Join them together in
+ * a new string variable str[].
+ */
+
+#include <stdio.h>
+
+void concatenateStrings(char dest[], const char src[]) {
+  int i, j;
+
+  // Find the length of dest
+  for (i = 0; dest[i] != '\0'; i++)
+    ;
+
+  // Append src to dest
+  for (j = 0; src[j] != '\0'; j++) {
+    dest[i + j] = src[j];
+  }
+  dest[i + j] = '\0';  // Add null terminator
+}
+
+int main() {
+  char str1[100], str2[100];
+  char concatenatedStr[200];
+
+  printf("Enter the first string: ");
+  scanf("%s", str1);
+
+  printf("Enter the second string: ");
+  scanf("%s", str2);
+
+  concatenateStrings(concatenatedStr, str1);
+  concatenateStrings(concatenatedStr, str2);
+
+  printf("Concatenated string: %s\n", concatenatedStr);
+
+  return 0;
+}
