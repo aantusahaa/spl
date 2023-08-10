d94d358f5d489a19b836d6673010f070bee3193b
Antu Saha <imantu09@gmail.com>
2023-08-10 17:37:40 +0600

    add: reverse number

---
+++ 027-reverse-number/main.c
@@ -0,0 +1,26 @@
+/**
+ * Problem: Reverse the given number
+ */
+
+#include <stdio.h>
+
+int reverseNumber(int num) {
+  int reversedNum = 0;
+  while (num > 0) {
+    int digit = num % 10;
+    reversedNum = reversedNum * 10 + digit;
+    num /= 10;
+  }
+  return reversedNum;
+}
+
+int main() {
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+
+  int reversed = reverseNumber(num);
+  printf("Reversed number: %d\n", reversed);
+
+  return 0;
+}
